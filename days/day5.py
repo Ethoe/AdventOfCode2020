b692a7a2d83ade3fae9c394fcd2a77b3f7733e40
Ethoe <ryanouttrim@gmail.com>
2020-12-05 10:11:25 -0700

    Updated because im dumb

--- days/day5.py
+++ days/day5.py
@@ -3,15 +3,15 @@ def decode(ticket):
     ticket = ticket.replace('B', '1')
     ticket = ticket.replace('R', '1')
     ticket = ticket.replace('L', '0')
-    return int(ticket[:-3], 2), int(ticket[7:], 2)
+    return int(ticket, 2)
 
 
 def p1(tickets):
-    return max([decode(ticket)[0] * 8 + decode(ticket)[1] for ticket in tickets])
+    return max([decode(ticket) for ticket in tickets])
 
 
 def p2(tickets):
-    seats = sorted([decode(ticket)[0] * 8 + decode(ticket)[1] for ticket in tickets])
+    seats = sorted([decode(ticket) for ticket in tickets])
     current = seats[0]
     for seat in range(1, len(seats)):
         if seats[seat] != current + 1:
